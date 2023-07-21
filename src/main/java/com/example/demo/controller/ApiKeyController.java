01f3f9d75233acd7383ed5b6c290db22f5ccfe1d
Yaroslav Storozhuk <yaroslav_storozhuk@epam.com>
2023-07-21 21:13:16 +0300

    Implement crud for api key

--- src/main/java/com/example/demo/controller/ApiKeyController.java
+++ src/main/java/com/example/demo/controller/ApiKeyController.java
@@ -8,8 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api-key")
@@ -25,27 +27,50 @@ public class ApiKeyController {
     }
 
     @PostMapping
-    public ApiKey save(@RequestBody ApiKey apiKey) {
-        if (apiKey.getId() != null) {
-            final Optional<ApiKey> byId = apiKeyRepo.findById(apiKey.getId());
-            if (byId.isPresent()) {
-                final List<UUID> clientsIds = apiKey.getClients().stream().map(Client::getId).toList();
-                final List<Client> existingClients = clientRepo.findAllById(clientsIds);
-                existingClients.forEach(c -> c.setApiKey(apiKey));
-                clientRepo.saveAll(existingClients);
-                return apiKeyRepo.save(apiKey);
-            } else {
-                throw new RuntimeException("API KEY does not exist");
-            }
+    public ApiKey create(@RequestBody ApiKey apiKey) {
+        final List<UUID> clientsIds = apiKey.getClients().stream().map(Client::getId).toList();
+        final List<Client> existingClients = clientRepo.findAllById(clientsIds);
+
+        if (existingClients.size() != apiKey.getClients().size()) {
+            throw new RuntimeException("Client does not exist");
+        }
+        existingClients.forEach(c -> c.setApiKey(apiKey));
+        apiKey.setClients(existingClients);
+        return apiKeyRepo.save(apiKey);
+    }
+
+    @PutMapping
+    public void update(@RequestBody ApiKey apiKey) {
+        final Optional<ApiKey> existingApiKey = apiKeyRepo.findById(apiKey.getId());
+        if (existingApiKey.isEmpty()) {
+            throw new RuntimeException("API-KEY does not exist");
+        }
+        final List<Client> clientsForUpdate = apiKey.getClients();
+        if (clientsForUpdate == null) {
+            return;
         }
 
-        final ApiKey saved = apiKeyRepo.save(apiKey);
-        final List<UUID> clientsIds = saved.getClients().stream().map(Client::getId).toList();
-        final List<Client> existingClients = clientRepo.findAllById(clientsIds);
-        existingClients.forEach(c -> c.setApiKey(saved));
-        clientRepo.saveAll(existingClients);
+        final List<Client> oldClients = clientRepo.findAllById(existingApiKey.get()
+                .getClients()
+                .stream()
+                .map(Client::getId)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList()));
 
-        return saved;
+        oldClients.forEach(c -> c.setApiKey(null));
+        clientRepo.saveAll(oldClients);
+
+        clientsForUpdate.forEach(c -> c.setApiKey(apiKey));
+        clientRepo.saveAll(clientsForUpdate);
+    }
+
+    @PatchMapping
+    public ApiKey patch(@RequestBody ApiKey apiKey) {
+        final Optional<ApiKey> existingApiKey = apiKeyRepo.findById(apiKey.getId());
+        if (existingApiKey.isEmpty()) {
+            throw new RuntimeException("API-KEY does not exist");
+        }
+        return apiKeyRepo.save(apiKey);
     }
 
     @DeleteMapping("/{id}")
@@ -55,6 +80,7 @@ public class ApiKeyController {
             ApiKey apiKey = byId.get();
             apiKey.getClients().forEach(c -> c.setApiKey(null));
             clientRepo.saveAll(apiKey.getClients());
+            apiKeyRepo.deleteById(apiKey.getId());
         }
     }
 }
